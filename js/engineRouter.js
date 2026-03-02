542474c5d3c88004e20e94c574be590a6e500342
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-02 17:32:31 +0530

    Update engineRouter.js

--- js/engineRouter.js
+++ js/engineRouter.js
@@ -1,1 +1,11 @@
+import { calculateSP as calculateFlipkartSP } from "./flipkartEngine.js";
+import { calculateMyntraSP } from "./myntraEngine.js";
 
+export function calculateSP(category, TP, mp, brand){
+
+  if(mp === "MYNTRA"){
+    return calculateMyntraSP(category, TP, brand);
+  }
+
+  return calculateFlipkartSP(category, TP);
+}
