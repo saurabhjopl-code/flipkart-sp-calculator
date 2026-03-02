280994ce92ddd83601c9fc43d8630a2e9c918999
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-02 18:25:07 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,17 @@
 import { loadAllData } from "./data.js";
-
 import { initUI } from "./ui.js";
 
-import { initFlipkartTables } from "./engines/flipkartEngine.js";
-import { initMyntraTables } from "./engines/myntraEngine.js";
+async function startApp() {
+  try {
 
-async function startApp(){
-
-  try{
-
-    // 1️⃣ Load all sheets (SKU + FK slabs + Myntra slabs)
     const data = await loadAllData();
 
-    // 2️⃣ Initialize Engines
-    initFlipkartTables(data);
-    initMyntraTables(data);
+    // pass FULL data object to UI
+    initUI(data);
 
-    // 3️⃣ Start UI with only SKU data
-    initUI(data.skuData);
-
-  }catch(error){
-
+  } catch (error) {
     console.error("App failed to start:", error);
-
   }
-
 }
 
 startApp();
