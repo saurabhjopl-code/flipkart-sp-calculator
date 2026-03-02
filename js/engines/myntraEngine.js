3016c9637a6b7c9a2f4273a1f565a62caa1b4e3e
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-02 16:18:47 +0530

    Update myntraEngine.js

--- js/engines/myntraEngine.js
+++ js/engines/myntraEngine.js
@@ -1,1 +1,22 @@
+export function calculateMyntraSP(category, TP){
 
+  // TODO: Add Myntra fee structure
+
+  let SP = TP * 1.3; // temporary logic
+
+  return {
+    SP,
+    Commission: 0,
+    Collection: 0,
+    Fixed: 0,
+    CommissionGST: 0,
+    CollectionGST: 0,
+    FixedGST: 0,
+    TDS: 0,
+    TCS: 0,
+    BankSettlement: TP,
+    InputGSTCredit: 0,
+    IncomeTaxCredit: 0,
+    EffectiveNet: TP
+  };
+}
