65cc43f46757f2f3e1db521e206a09754a423dc3
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-02 16:18:03 +0530

    Update engineRouter.js

--- js/engines/engineRouter.js
+++ js/engines/engineRouter.js
@@ -1,1 +1,12 @@
+import { calculateFlipkartSP } from "./flipkartEngine.js";
+import { calculateMyntraSP } from "./myntraEngine.js";
 
+export function calculateSP(category, TP, mp){
+
+  if(mp === "MYNTRA"){
+    return calculateMyntraSP(category, TP);
+  }
+
+  // FLIPKART + SHOPSY handled in same engine
+  return calculateFlipkartSP(category, TP);
+}
